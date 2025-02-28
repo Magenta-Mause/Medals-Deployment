fbf86a607f636a08990e7037640f561edb858154
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-02-28 18:03:21 +0100

    prepare for pr

--- db_backup/backup.sh
+++ db_backup/backup.sh
@@ -7,5 +7,4 @@ pg_dump -U $POSTGRES_USER -h postgres_db $POSTGRES_DB > $BACKUP_FILE
 echo "Backup created at $BACKUP_FILE"
 
 # Remove backups older than 90 days
-# find backups -type f -name "*.sql" -mtime +1 -exec rm {} \;
-find backups -type f -name "*.sql" -mmin +1 -exec sh -c 'echo "Deleting {}"; rm {}' \;
+find backups -type f -name "*.sql" -mtime +90 -exec sh -c 'echo "Deleting {}"; rm {}' \;
--- db_backup/db-backup.Dockerfile
+++ db_backup/db-backup.Dockerfile
@@ -1,5 +1,5 @@
 FROM postgres:17
 COPY backup.sh /usr/local/bin/backup.sh
 RUN chmod +x /usr/local/bin/backup.sh
-# Rerun backup every 30 days (2592000 seconds)
-CMD ["sh", "-c", "while true; do /usr/local/bin/backup.sh; sleep 40; done"]
+# Rerun backup every 7 days (604.800 seconds)
+CMD ["sh", "-c", "while true; do /usr/local/bin/backup.sh; sleep 604.800; done"]
