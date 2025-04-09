013153475f0bdabf140a62693d6b7b6cf478eb9d
EcoFreshKase <alexander.jablo@gmail.com>
2025-04-09 11:40:20 +0200

    move backup script into postgres_db container

--- dbBackup.sh
+++ dbBackup.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-DEPLOY_ENV_PATH=${1}
-BACKUP_DIR=${2}
-if [ ! -f "$DEPLOY_ENV_PATH" ]; then
-  echo "Error: DEPLOY_ENV_PATH not provided" >&2
-  exit 1
-fi
-if [ ! -f "$DEPLOY_ENV_PATH" ]; then
-  echo "Error: BACKUP_DIR not provided" >&2
-  exit 1
-fi
-
+BACKUP_DIR="/backups"
 BACKUP_FILE="$BACKUP_DIR/backup_$(date +%Y_%m_%d_%H_%M_%S).sql"
-
-# Ensure the backup directory exists
-mkdir -p $BACKUP_DIR
-
-# Load environment variables from the specified deploy.env file
-set -a
-  source "$DEPLOY_ENV_PATH"
-set +a
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 pg_dump -U $POSTGRES_USER -h localhost $POSTGRES_DB > $BACKUP_FILE
