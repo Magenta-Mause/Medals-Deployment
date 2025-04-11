1c6a870644a9d644ea0f46a1443337ab138c1701
fietensen <fietensen@gmail.com>
2025-04-11 13:45:24 +0200

    fix bug

--- scripts/log_purge_current.sh
+++ scripts/log_purge_current.sh
@@ -3,5 +3,5 @@
 # Syntax: ./log_purge_current.sh [container_id]
 current_time=$(date --utc +"%Y-%m-%dT%H:%M:%S.%NZ")
 echo "Purging all logs for container $1 before timestamp $current_time"
-./purge_before.sh $1 $current_time
+./log_purge_before.sh $1 $current_time
 echo "Done."
